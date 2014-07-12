f0889cb6532fc8e2a03b08602e505b0545b46520
shah-hardik <hardiks059@gmail.com>
2014-07-12 14:24:12 +0530

    ok

--- instance/front/controller/login.inc.php
+++ instance/front/controller/login.inc.php
@@ -12,34 +12,26 @@
 $urlArgs = _cg("url_vars");
 session_start();
 
-
-
 if (isset($_REQUEST['sbt_btn'])) {
 
 
-    
-    
-    
     $email = _escape($_REQUEST['email']);
     $password = _escape($_REQUEST['password']);
 
     $data = q("select email ,password from registration where email = '$email'  AND password = '$password' ");
-    $id1= qs("select username from registration where email = '$email' ");
-   // d($data); 
+    $id1 = qs("select username from registration where email = '$email' ");
+    // d($data); 
     //d($id1);
-    
- $_SESSION['user'] =$id1;
-       // d($_SESSION['user']);
+
+    $_SESSION['user'] = $id1;
+    // d($_SESSION['user']);
 
     if ($data != null) {
-       
-       _R('view_request');
+
+        _R('view_request');
     } else {
 
         $error = "Username Or Password is Wrong!!!";
     }
 }
-
-
-
 ?>
